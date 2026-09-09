035326b35f70817245cc1ab78155b4abe24b9d65
Bastien Metayer <bastien.metayer@angers.globalopencampus.com>
2026-09-09 14:27:19 +0200

    durée timer dans annonces.json

--- frontend/annonce.js
+++ frontend/annonce.js
@@ -64,7 +64,10 @@ function afficherDetailsAnnonce(id) {
 
   document.getElementById('btn-retour').addEventListener('click', retourALaListe);
   document.getElementById('open-form-button')
-        .addEventListener('click', () => window.open("form_page.html", "_blank"));
+    .addEventListener('click', () => {
+        localStorage.setItem('annonceTime', annonce.time);
+        window.open("form_page.html", "_blank");
+    });
 }
 
 function retourALaListe() {
